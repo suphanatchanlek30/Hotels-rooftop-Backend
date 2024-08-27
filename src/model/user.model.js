3005743e1a409eb13177225546982daa9ff72e57
suphanatchanlek <suphanatchanlek@gmail.com>
2024-08-27 18:13:26 +0700

    add function hash password before saving to database

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     username: {
@@ -23,6 +24,16 @@ const userSchema = new Schema({
         type: Date,
         default: Date.now
     }
+});
+
+// hash password before saving to database
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if(!user.isModified('password')) return next();
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+    next();
+
 });
 
 const User = model("User", userSchema);
