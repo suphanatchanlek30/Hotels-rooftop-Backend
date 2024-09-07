882131ad775ad16245b1650b6ab4f24407b8c1d6
suphanatchanlek <suphanatchanlek@gmail.com>
2024-09-07 13:15:57 +0700

    add code Update a User Role

--- src/routes/auth.user.route.js
+++ src/routes/auth.user.route.js
@@ -103,4 +103,25 @@ router.delete('/users/:id', async (req, res) => {
     }
 });
 
+// System Update a user role
+router.put('/users/:id', async (req, res) =>{
+    try {
+
+        const {id} = req.params;
+        const {role} = req.body;
+        const user = await User.findByIdAndUpdate(id, {role}, {new: true});
+
+        if(!user) {
+            return res.status(404).send({ message: 'User not found'});
+        }
+
+        res.status(200).send({message: "User role updated successfully", user});
+
+    } catch (error) {
+        console.error("Error updating user role", error);
+        res.status(500).json({ message: "Failed updating user role!" });
+    }
+});
+
+
 module.exports = router;
