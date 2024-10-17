a912efe5fe9dd73ee5b868a0c3ad8183cc2a2809
suphanatchanlek <suphanatchanlek@gmail.com>
2024-10-17 19:28:58 +0700

    test

--- src/middleware/isAdmin.js
+++ src/middleware/isAdmin.js
@@ -2,7 +2,7 @@ const isAdmin = (req, res, next) => {
     if(req.role !== 'admin'){
         return res.status(403).send({success: false, message: 'You are not allowed to to perfrorm this action. Please try to login as an admin.' });
     }
-
+    // test
     next();
 }
 
